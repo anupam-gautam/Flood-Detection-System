46f2cd2a3df55f8ad80161c73e88863a252a5f5c
anupam-gautam <anupam.gautam46@gmail.com>
2022-12-28 23:10:53 +0545

    Random Forest Initialized but code not working

--- Flood Detection System/Controllers/WeatherDataController.cs
+++ Flood Detection System/Controllers/WeatherDataController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Flood_Detection_System.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RestSharp;
 using System.Net;
+using System.Text.Json;
 
 namespace Flood_Detection_System.Controllers
 {
@@ -10,45 +12,24 @@ namespace Flood_Detection_System.Controllers
 
     public class WeatherDataController : ControllerBase
     {
-        [HttpGet("Dummy")]
-        public object OpenWeather()
+
+        [HttpPost]
+        [Route("WeatherApi")]
+        public object WeatherApi(WeatherApiInputOutputModel weatherApiInputModel)
         {
-
-            //string apiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=55c4e47fe289c1dda9c9a7acfba40ce5";
-            string apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=3.4653&lon=62.2159&appid=55c4e47fe289c1dda9c9a7acfba40ce5";
-            //string apiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=541414&appid=55c4e47fe289c1dda9c9a7acfba40ce5";
-
-
-            RestClient client = new RestClient(apiUrl);
-            //var request = new RestRequest(action, method);
-            var request = new RestRequest(apiUrl);
-            request.RequestFormat = DataFormat.Json;
-
-            //request.AddParameter("application/json", jsonObject, ParameterType.RequestBody);
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-            var response = client.Execute(request);
-            return response.Content;
-        }
-
-        [HttpGet]
-        public object WeatherApi()
-        {
-
-            string apiUrl = "https://api.weatherapi.com/v1/forecast.json?key=068bfabb64a443f592b152849222712&q=48.8567,2.3508&days=1&aqi=no&alerts=yes";
+            weatherApiInputModel.lat = 50.238422;
+            weatherApiInputModel.lon = -94.438956;
+            string aqi = weatherApiInputModel.airQualityIndex ? "yes" : "no";
+            string alerts = weatherApiInputModel.airQualityIndex ? "yes" : "no";
+            string apiUrl = "https://api.weatherapi.com/v1/forecast.json?key=068bfabb64a443f592b152849222712&q=" + weatherApiInputModel.lat + "," + weatherApiInputModel.lon + "&days=" + weatherApiInputModel.forecastDays + "&aqi=" + aqi + "&alerts=" + alerts;
             RestClient client = new RestClient(apiUrl);
             var request = new RestRequest(apiUrl);
             request.RequestFormat = DataFormat.Json;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
             var response = client.Execute(request);
-            return response.Content;
+            var res = JsonSerializer.Deserialize<WeatherOutput>(response.Content);
+            return res;
         }
-        //public string GetWeatherData()
-        //{
-        //    object response = new object();
-        //    response = APIHeko
-
-        //    return Ok(response);
-
-        //}
+   
     }
 }
---
+++ Flood Detection System/Models/WeatherApiInputModel.cs
@@ -0,0 +1,43 @@
+﻿namespace Flood_Detection_System.Models
+{
+    public class WeatherApiInputOutputModel
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public int forecastDays { get; set; }
+        public bool airQualityIndex { get; set; }
+        public bool alert { get; set; }
+    }
+
+    public class WeatherOutput
+    {
+        public WeatherLocation location { get; set; }
+        public WeatherForecast forecast { get; set; }
+    }
+
+    public class WeatherLocation
+    {
+        public string name { get; set; }
+        public string region { get; set; }
+        public string localtime { get; set; }
+    }
+
+    public class WeatherForecast
+    {
+        public List<ForecastDay> forecastday { get; set; }
+    }
+
+    public class ForecastDay
+    {
+        public Day day { get; set; }
+    }
+
+    public class Day
+    {
+        public float maxtemp_c { get; set; }
+        public float mintemp_c { get; set; }
+        public float totalprecip_mm { get; set; }
+    }
+}
+
+
